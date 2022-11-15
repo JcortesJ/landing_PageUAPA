44d7785b47128ab034d44d9e7018f9ca25d680b0
juan <jcortesj@unal.edu.co>
2022-11-15 00:11:56 -0500

    intentando arreglar textarea

--- landinguapa/components/formU.tsx
+++ landinguapa/components/formU.tsx
@@ -8,7 +8,7 @@ const Form = () =>{
             <h3><strong>Contact us</strong></h3>
             <input placeholder="Your name" className={styles.inputNormal}></input>
             <input placeholder="Your email" className={styles.inputNormal}></input>
-            <input placeholder="Your message" className={styles.inputGrande} type={"textarea"} ></input>
+            <textarea placeholder="Your message" className={styles.inputGrande}  ></textarea>
             <button className={styles.botonFancy2}>
             Send
           </button>
